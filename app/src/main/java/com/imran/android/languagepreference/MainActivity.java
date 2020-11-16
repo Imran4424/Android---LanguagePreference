e91342d26f126aea4f9cc5764cac6f19a66ecc23
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-11-16 23:45:23 +0600

    dialog

--- app/src/main/java/com/imran/android/languagepreference/MainActivity.java
+++ app/src/main/java/com/imran/android/languagepreference/MainActivity.java
@@ -1,7 +1,9 @@
 package com.imran.android.languagepreference;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new AlertDialog(this)
+                .setIcon(android.R.drawable.ic_btn_speak_now)
+                .setTitle("Choose a language")
+                .setMessage("Which language would you like to use")
+                .setPositiveButton("English", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Set English
+                    }
+                })
+                .setNegativeButton("Bengali", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Set Bengali
+                    }
+                });
     }
 }
